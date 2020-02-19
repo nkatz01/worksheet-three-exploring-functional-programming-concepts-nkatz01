dface202ab614dbbf982111f2b37f59227bf62c3
=nkatz01 <=>
2020-02-19 07:18:16 -0800

    solving #5

--- wksht3/wksht3/Program.cs
+++ wksht3/wksht3/Program.cs
@@ -11,14 +11,28 @@ using System.Reflection;
 
 namespace wksht3Solutions
 {
+	/* public class Example{
+		List<int> numbers2 = new List<int>() { 2, 3, 5, 7 };
+		  Func<int, int> addOne = n => n + 1;
+		Func<int, int> multiply = n => n * 2;
+		Func<int, int> subtractOne = n => n - 1;
+		Action<List<int>> print = n => n.ForEach(i => Console.Write(" " + i.ToString()));
+	
+		
+	} */
     public class Program
     {
-
-
+		
+	 List<int> numbers2 = new List<int>() { 2, 3, 5, 7 };
+		  Func<int, int> addOne = n => n + 1;
+		Func<int, int> multiply = n => n * 2;
+		Func<int, int> subtractOne = n => n - 1;
+		Action<List<int>> print = n => n.ForEach(i => Console.Write(" " + i.ToString()));
+	
 		
 		public static void Main(String[] args)
 		{
-			List<int> ls = new List<int>();
+			/* List<int> ls = new List<int>();
 			string userInput = "";
 			int intVal;
 
@@ -32,10 +46,20 @@ namespace wksht3Solutions
 					ls.Add(intVal);
 
 				// Console.WriteLine("You entered  ",ls.ToString());
-			}
+			} */
+			Program prg = new Program();
+			prg.Fire();
+		}
+	public void Fire()
+        {
+				
+ 	     FieldInfo field = this.GetType().GetField("addOne", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+		Delegate method = field.GetValue(this) as Delegate;
+	var val =	method.Method.Invoke(method.Target, new object[1]{10});
+Console.WriteLine(val);
+		}
 
-
-
+		
 			//List<int> funcs = new List<int>();
 			//while (userInput != "end")
 			//{
@@ -67,59 +91,31 @@ namespace wksht3Solutions
 
 
 
-			//	enum Operations
-			//{ 
-			//	 addOne,
-			//	 multiply,
-			//	subtractOne,
-			//	print
-			//}
+		 
 
-			Program prg = new Program();
+			//Program prg = new Program();
 			 
 			//print(ls);
-			//Func<int, int, int> addNums = (x, y) => x + y;
+			 
 			//Func<int, bool> isZero = n => n == 0;
 
-
+/* typeof(Container)
+    .GetMethod(cmd, BindingFlags.Static | BindingFlags.Public)
+    .Invoke(null, new object[0]); */
 		
 		 
 		 
 
 		 
 
-			 Type ex = typeof(Example);
-			 var method = ex.GetMethod("addOne");
-			//MethodInfo method = ex.GetMethod("addOne");
-			// tm.Invoke(this, new object[] { 1, 3, 4 });
-			//object[] parametersArray = new object[] { 1};
+			  
 
-			//	 DisplayGenericMethodInfo(method);
-			//MethodInfo gm = method.MakeGenericMethod(typeof(int));
-			object[] arg = { 42 };
-		//	gm.Invoke(this, args);
-			Console.WriteLine(method.Invoke(ex, arg));
-		 
-	//	object[] arr = new object[] { new object[] { input } };
-
-
-		}
+		//}
 		
 		// public static Func<List<int>, int> returnMinFrmCollec = n => n.Min();
 	
 
     }
 
-
-	public class Example {
-
-		public List<int> numbers2 = new List<int>() { 2, 3, 5, 7 };
-		public static Func<int, int> addOne = n => n + 1;
-		Func<int, int> multiply = n => n * 2;
-		Func<int, int> subtractOne = n => n - 1;
-		Action<List<int>> print = n => n.ForEach(n => Console.Write(" " + n.ToString()));
-
-
-
-	}
+ 
 }
