c71972d4ad978515f75f07a7ecf8ea5de9edf81a
unknown <Nuchem@CS00041648.ad.ucl.ac.uk>
2020-02-21 16:29:22 +0000

    problem 10 solved - need to implement processing input and generlizing it with problem 5

--- wksht3/wksht3/Program.cs
+++ wksht3/wksht3/Program.cs
@@ -125,9 +125,7 @@ namespace wksht3Solutions
 		static Func<List<int>, Predicate<int>, List<int>> predTester = (n, p) => n.Where(i => p(i) == true).Select(i => i).Reverse().ToList();
 		static Func<IEnumerable<string>, string, IEnumerable<string>> itemConcatenater = (n, s) => n.Select(i => i = s + i);
 		static Action<List<int>> print = n => n.ForEach(i => Console.Write(" " + i.ToString()));
-		//Func<List<int>, List<int>> addOne = n => n.Select(i => i + 1).ToList();
-		//  Func<List<int>, List<int>> multiply = n => n.Select(i => i * 2).ToList();
-		//  Func<List<int>, List<int>> subtractOne = n => n.Select(i => i - 1).ToList();
+		 
 		static Func<int, List<int>> returnList = n => Enumerable.Range(0, n).ToList();
 	
 		public static void Main(String[] args)
