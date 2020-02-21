03c4a4ef664df39829976ba741d1c82bbe54c425
unknown <Nuchem@CS00041648.ad.ucl.ac.uk>
2020-02-21 16:26:53 +0000

    problem 10 solved - need to implement processing input and generlizing it with problem 5

--- wksht3/wksht3/Program.cs
+++ wksht3/wksht3/Program.cs
@@ -10,84 +10,137 @@ using System.Reflection;
 using System.Data;
 using System.Linq.Expressions;
 using System.Runtime;
- 
+using System.Text;
 
 namespace wksht3Solutions
-{
+{	
 	
+	
+	public static class ExtClass {
+		public static string ModifyString(this string source, int multiplier)//https://stackoverflow.com/questions/532892/can-i-multiply-a-string-in-c/532912#532912
+		{
+			
+			if (multiplier > 0)
+			{
+				StringBuilder sb = new StringBuilder(multiplier * source.Length);
+
+				for (int i = 0; i < multiplier; i++)
+				{
+					sb.Append(source+",");
+				}
+				sb.Remove(sb.Length-1, 1);
+				return sb.ToString();
+			}
+			else
+			{
+				  return source.Replace(source, "");
+			//	sb.Insert(0, "Rmoved");
+			}
+			
+		}
+
+	}
+
+
 	public class Program
 	{
 		public delegate bool Del1(string str);
-		//public delegate int Del2(int i, int j);
-		//public delegate string Del3(string s);
-		//static Func<List<string>, string, int> removeAllstartswith = (n, s) => n.RemoveAll(i => i.StartsWith(s)==true);
-		// static Func<List<string>, string, int> removeAllendsswith = (n, s) => n.RemoveAll(i => i.EndsWith(s) == true);
-		//  Func<Func<string, bool>,string, string, bool> p = (strtOrEnd,  s1, s2) => Del(s2) == true;
-		static Func<List<string>, string, string, int> removeAllstartsOrEndsWith = (n, s ,funcName) => n.RemoveAll(i => myStartOrEndWith(funcName, i, s) == true);
-		static Func<List<string>, string, string, int> copyAllstartsOrEndsWith = (n, s, funcName) => n.Insert(i => myStartWith(funcName, i, s) == true);
+		
+		static Func<List<int>, List<int>> addOne = n => n.Select(i => Applyoperator(i, "+")).ToList();
+		static Func<List<int>, List<int>> multiply = n => n.Select(i => Applyoperator(i, "*")).ToList();
+		static Func<List<int>, List<int>> subtractOne = n => n.Select(i => Applyoperator(i, "-")).ToList();
 
-		Func<IEnumerable<string>, int,bool, List<string>> filterNamesWithGivenLen = (l, n, t) => l.Where(i => i.Length = n).Select(i => myLengthRemovOrDoublr().ToList();
+		static Func<List<string>, string, string, IEnumerable<string>> Double = (n, condition, s) => n.Select(i =>  conditionTester(condition, i, s) == true ? i = Applyoperator(i, "Double") : i=i ).ToList();
 
-		static public bool myStartOrEndWith(string nameOfFunc, string s1, string s2)
+
+	 	static Func<List<string>, string, string, int> Remove = (n, condition, s) => n.RemoveAll(i => conditionTester(condition, i, s) == true);
+
+
+		static public bool conditionTester(string nameOfCond, string s1, string s2)
 		{
-			Del1 strtOrEndWith;
-			Delegate del = Delegate.CreateDelegate(typeof(Del1), s1, nameOfFunc);
-			strtOrEndWith =(Del1) del;
-			return  strtOrEndWith(s2);
+			int property;
+ 			if (int.TryParse(s2, out property))
+			{
+				
+				return (int)typeof(string).GetProperty(nameOfCond).GetValue(s1, null) == property;
+			}
+			else
+			{
+				Del1 condition;
+				Delegate del = Delegate.CreateDelegate(typeof(Del1), s1, nameOfCond );
+				condition = (Del1)del;
+				return condition(s2);
+			}
+
+			
 		}
 
-		static public bool myLengthRemovOrDoublr(string nameOfFunc, string s1, string s2)
+		 
+
+		
+
+		static public T Applyoperator<T>(T v, string symbol)  //https://stackoverflow.com/questions/756954/arithmetic-operator-overloading-for-a-generic-class-in-c-sharp
 		{
-			Del1 strtOrEndWith;
-			Delegate del = Delegate.CreateDelegate(typeof(Del1), s1, nameOfFunc);
-			strtOrEndWith = (Del1)del;
-			return strtOrEndWith(s2);
+			switch (typeof(T).Name)
+			{
+
+				case "Int32":
+					{
+						switch (symbol)
+						{
+							case "+": return (T)(object)((int)(object)v + 1) ;
+							case "-": return (T)(object)((int)(object)v - 1) ;
+							case "*": return (T)(object)((int)(object)v * 2 );
+							default:
+								throw new InvalidOperationException("Unknown symbol");
+						}
+					 
+					}
+					 
+				
+				case "String":
+					{
+
+						switch (symbol)
+						{
+ 							case "Remove": return (T)(object)ExtClass.ModifyString((string)(object)v,-1 ) ;
+							case "Double": return (T)(object)ExtClass.ModifyString((string)(object)v, 2 ) ;
+							default:
+								throw new InvalidOperationException("Unknown symbol");
+						}
+
+					}
+					 
+
+				default:
+					throw new InvalidOperationException("Unknown type");
+			}
+
 		}
-		//static public int Applyoperator(int i, int j , string symbol)
-		//	{
 
-		//		Del2 arithmeticSymbol;
-		//		Delegate del = Delegate.CreateDelegate(typeof(Del2), exp, symbol);
-		//		arithmeticSymbol = (Del2)del;
-		//		return arithmeticSymbol(i, j).Compile();
-		//	}
-		//static public string Applyoperator(string s1, string s2, string symbol)
-		//{
-		//	Del3 stringOperand;
-		//	Delegate del = Delegate.CreateDelegate(typeof(Del3), s1, symbol);
-		//	stringOperand = (Del3)del;
-		//	return (s2);
-		//}
 
-		//public void applyOperator() {
-		//	Func<List<T>, List<T>> addOne = n => n.Select(i => i + 1).ToList();
-		//	List<int>  lsInts = new List<int> { 1, 2, 3 };
-		//	List<string> lsStrs = new List<string> { "hi", "itchy", "kitchy" }; 
-		//	 List<int> lsIntsProc = addOne<int>(lsInts);
-		//	  List<string> lststrProc = addOne<string>(lsStrs);
-
-		//}
+			
 
 
 		static Func<List<int>, Predicate<int>, List<int>> predTester = (n, p) => n.Where(i => p(i) == true).Select(i => i).Reverse().ToList();
 		static Func<IEnumerable<string>, string, IEnumerable<string>> itemConcatenater = (n, s) => n.Select(i => i = s + i);
 		static Action<List<int>> print = n => n.ForEach(i => Console.Write(" " + i.ToString()));
-		Func<List<int>, List<int>> addOne = n => n.Select(i => i + 1).ToList();
-		  Func<List<int>, List<int>> multiply = n => n.Select(i => i * 2).ToList();
-		  Func<List<int>, List<int>> subtractOne = n => n.Select(i => i - 1).ToList();
+		//Func<List<int>, List<int>> addOne = n => n.Select(i => i + 1).ToList();
+		//  Func<List<int>, List<int>> multiply = n => n.Select(i => i * 2).ToList();
+		//  Func<List<int>, List<int>> subtractOne = n => n.Select(i => i - 1).ToList();
 		static Func<int, List<int>> returnList = n => Enumerable.Range(0, n).ToList();
 	
 		public static void Main(String[] args)
 		{
+			List<int> nums = new List<int>() { 1, 2, 3, 4 };
 			List<string> ls = new List<string>() { "Kurnelia", "Qnaki", "Geo", "Muk", "Ivan" };
-			removeAllstartsOrEndsWith(ls,"eo","EndsWith");
-			 Console.WriteLine("[{0}]", string.Join(",", ls));
-
-			//string MethodName = "StartsWith";
-
-			// Console.WriteLine(Applyoperator(1, 2, "Add"));
 		
-			 Console.WriteLine(addOne("hey");
+			// IEnumerable<string> newls = Double(ls, "Length", "5");
+			//Console.WriteLine("[{0}]", string.Join(",", newls));
+
+
+			Remove(ls, "Length", "5");
+			Console.WriteLine("[{0}]", string.Join(",", ls));
 
 
 
@@ -95,79 +148,68 @@ namespace wksht3Solutions
 
 
 
-			//	Del myStartsWith;
-			//Delegate test1 = Delegate.CreateDelegate(typeof(Del), "abc", "StartsWith");
-			// myStartsWith =(Del) test1;
-			//Console.WriteLine(myStartsWith("abc"));
-			//Delegate test2 = Delegate.CreateDelegate(typeof(Del), "abc", "EndsWith");
-
-			//	Console.WriteLine(test != null);
-
-			//foreach(MethodInfo mi in Iter(meth))
-			//{
-			//	Console.WriteLine(mi);
-			//}
-			//	 Delegate method = memb.GetValue(0) as ;
-			//Console.WriteLine(meth[0].GetType());
 
 
 
 
 
-			// 	int[] ar = { 1, 2, 3, 4, 5, 6 };
-			//List<int> nums = new List<int>() { 1, 2, 3, 4 };
-			//List<string> commands = new List<string>() { "ddOne"};
+
+			//	int[] ar = { 1, 2, 3, 4, 5, 6 };
+
+			List<string> commands = new List<string>();// { "addOne" };
+			List<int> parameters = new List<int>();
+			string userInput = "";
+			int intVal;
 
 
-			//List<int> parameters = new List<int>();
-			//string userInput = "";
-			//int intVal;
+			while (userInput != "end")
+			{
+
+				Console.Write("Enter integer or string value, press 'end' to stop: ");
+				userInput = Console.ReadLine();
+				if (int.TryParse(userInput, out intVal))
+					parameters.Add(intVal);
+
+			}
+			userInput = "";
+			Program prg = new Program();
+			FieldInfo field;
+			List<string> funcLst = new List<string>();
+			while (userInput != "end")
+			{
+				Console.Write("Enter function: eg 'addOne', 'multiply', 'subtractOne'; and press 'end' to stop: ");
+				userInput = Console.ReadLine();
+				field = prg.GetType().GetField(userInput, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+				if (field != null)
+				{
+					funcLst.Add(userInput);
+
+				}
+				else
+				{
+					Console.WriteLine("The command you entered is not recognised");
+				}
+
+			}
+
+			prg.Execute(funcLst, parameters);
 
 
-			//while (userInput != "end")
-			//{
-
-			//	Console.Write("Enter integer or string value, press 'end' to stop: ");
-			//	userInput = Console.ReadLine();
-			//	if (int.TryParse(userInput, out intVal))
-			//		parameters.Add(intVal);
-
-			//}
-			//userInput = "";
-			//Program prg = new Program();
-			//FieldInfo field;
-			//List<string> funcLst = new List<string>();
-			//while (userInput != "end")
-			//{
-			//	Console.Write("Enter function: eg 'addOne', 'multiply', 'subtractOne'; and press 'end' to stop: ");
-			//	userInput = Console.ReadLine();
-			//	 field = prg.GetType().GetField(userInput ,BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-			//	if (field!=null) {
-			//			funcLst.Add(userInput );
-
-			//	}
-			//	else
-			//	{
-			//		Console.WriteLine("The command you entered is not recognised");
-			//	}
-
-			//}
-
-			//prg.Execute(funcLst, parameters);
 
 
-			//	try {
-			//		FieldInfo field = prg.GetType().GetField(commands[0]);
 
-			//	prg.Execute(commands, nums);
-			//}
-			//	catch (Exception Ex)
-			//	{
-			//		Console.WriteLine("The command you entered is not recognised");
-			//	}
+
 
 		}
+
+		//	public  T[][] ProcessInput<T>() {
+
+		//		T[][] array = commandsAndData = new Array(
+
+
+
+		//}
 
 
 		public void Execute(List<string> funcLst, List<int> parameters)//https://stackoverflow.com/questions/6010555/how-to-call-delegate-from-string-in-c
@@ -188,16 +230,24 @@ namespace wksht3Solutions
 
 		}
 		
-		static IEnumerable<MethodInfo>  Iter(IEnumerable<MethodInfo> collec)
+		static IEnumerable<int>  Iter(IEnumerable<int> collec)
 		{
-			foreach (MethodInfo n in collec)
+			foreach (int n in collec)
 			{
 			    yield return n;
 			}
 		}
 
+		static IEnumerable<string> Iter(IEnumerable<string> collec)
+		{
+			foreach (string n in collec)
+			{
+				yield return n;
+			}
+		}
 
-	 
+
+
 	}
 
  
